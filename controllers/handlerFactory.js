07f43da145e93e1819a738a02b8e4f9c61a208f4
hatran216004 <qiqisokiu.321@gmail.com>
2025-03-20 13:53:35 +0700

    add reviews pagination with tour populate

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,8 +1,9 @@
+const Review = require('../models/reviewModel');
 const ApiFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
-const { NOT_FOUND } = require('../utils/constants');
 const { filterObj } = require('../utils/helpers');
+const { NOT_FOUND, PAGE_LIMIT } = require('../utils/constants');
 
 exports.getAll = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -24,25 +25,57 @@ exports.getAll = (Model) =>
 
     const modelName = Model.modelName.toLowerCase();
 
+    const totalPages = Math.ceil(
+      totalDocuments / parseInt(req.query.limit, 10) || PAGE_LIMIT
+    );
+
     res.status(200).json({
       status: 'success',
       data: {
         [modelName]: docs
       },
-      pagination: { totalDocuments }
+      pagination: { total: totalDocuments, totalPages }
     });
   });
 
 exports.getOne = (Model, popOptions) =>
   catchAsync(async (req, res, next) => {
-    let query = Model.findById(req.params.id);
-    if (popOptions) query = query.populate(popOptions);
+    const { id } = req.params;
+    let query = Model.findById(id);
+
+    const modelName = Model.modelName.toLowerCase();
+
+    if (popOptions) {
+      const docs = await query;
+      const features = new ApiFeatures(Review.find({ tour: id }), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+      const reviews = await features.query;
+
+      // Tính tổng số reviews để hỗ trợ phân trang
+      const totalReviews = await Review.countDocuments({ tour: id });
+      const totalPages = Math.ceil(
+        totalReviews / parseInt(req.query.limit, 10) || PAGE_LIMIT
+      );
+      docs.reviews = reviews;
+
+      return res.status(200).json({
+        status: 'success',
+        pagination: {
+          total: totalReviews,
+          totalPages
+        },
+        data: {
+          [modelName]: docs
+        }
+      });
+    }
 
     const doc = await query;
     if (!doc)
       return next(new AppError('No document found with that ID', NOT_FOUND));
-
-    const modelName = Model.modelName.toLowerCase();
 
     res.status(200).json({
       status: 'success',
